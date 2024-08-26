7cfa19a893cc7e51166fdc86363b51297be611d9
eoruadl <108312272+eoruadl@users.noreply.github.com>
2024-08-26 22:26:10 +0900

    init

---
+++ src/main/java/com/eoboard/EoboardApplication.java
@@ -0,0 +1,13 @@
+package com.eoboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EoboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EoboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eoboard/EoboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eoboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EoboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
