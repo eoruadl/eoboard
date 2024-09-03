4de3fd50a3c9836dda1d4971787a358aecd421f2
eoruadl <108312272+eoruadl@users.noreply.github.com>
2024-09-04 00:30:53 +0900

    feat: JWT 토큰 에러 핸들링

--- src/main/java/com/eoboard/security/JwtAuthorizationFilter.java
+++ src/main/java/com/eoboard/security/JwtAuthorizationFilter.java
@@ -2,10 +2,18 @@ package com.eoboard.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.SignatureGenerationException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.eoboard.security.handler.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -38,7 +46,26 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
             .asString();
 
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            ErrorResponse errorResponse = new ErrorResponse();
+            ObjectMapper om = new ObjectMapper();
+            String errorMessage = "";
+
+            if (e instanceof TokenExpiredException) {
+                errorMessage = "토큰이 만료되었습니다.";
+            } else if (e instanceof JWTDecodeException) {
+                errorMessage = "토큰이 잘못되었습니다.";
+            } else if (e instanceof SignatureVerificationException) {
+                errorMessage = "토큰이 잘못되었습니다.";
+            }
+
+            errorResponse.setMessage(errorMessage);
+            errorResponse.setErrorCode(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            String jsonErrorResponse = om.writeValueAsString(errorResponse);
+            response.setCharacterEncoding("utf-8");
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write(jsonErrorResponse);
+
         }
     }
 }
