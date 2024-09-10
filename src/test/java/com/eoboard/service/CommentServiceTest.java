b186f48d0a03f182a1f648c495fb7ff0ffa3bec8
eoruadl <108312272+eoruadl@users.noreply.github.com>
2024-09-11 00:21:15 +0900

    feat: Post, Comment 테스트 코드 작성

---
+++ src/test/java/com/eoboard/service/CommentServiceTest.java
@@ -0,0 +1,95 @@
+package com.eoboard.service;
+
+import com.eoboard.domain.Comment;
+import com.eoboard.domain.Member;
+import com.eoboard.repository.CommentRepository;
+import com.eoboard.repository.PostRepository;
+import com.eoboard.repository.post.query.PostCommentQueryDto;
+import com.eoboard.repository.post.query.PostQueryRepository;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+public class CommentServiceTest {
+
+    @Autowired
+    PostService postService;
+    @Autowired
+    CommentService commentService;
+    @Autowired
+    CommentRepository commentRepository;
+    @Autowired
+    PostQueryRepository postQueryRepository;
+    @Autowired
+    EntityManager em;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Test
+    public void 댓글_작성() throws Exception {
+        //given
+        Member member = createMember();
+        Long postId = postService.post(member.getId(), "test1", "게시물1", "게시물 작성합니다.");
+        Long commentId = commentService.comment(member.getMemberId(), postId, "댓글을 작성합니다.");
+
+        //when
+        List<PostCommentQueryDto> comments = postQueryRepository.findComments(postId);
+
+        //then
+        assertEquals(commentId, comments.get(0).getCommentId());
+        assertEquals("nick", comments.get(0).getNickName());
+        assertEquals("댓글을 작성합니다.", comments.get(0).getContent());
+    }
+
+    @Test
+    public void 댓글_수정() throws Exception {
+        //given
+        Member member = createMember();
+        Long postId = postService.post(member.getId(), "test1", "게시물1", "게시물 작성합니다.");
+        Long commentId = commentService.comment(member.getMemberId(), postId, "댓글을 작성합니다.");
+
+        //when
+        commentService.updateComment(commentId, "댓글을 수정합니다.");
+        List<PostCommentQueryDto> comments = postQueryRepository.findComments(postId);
+
+        //then
+        assertEquals(commentId, comments.get(0).getCommentId());
+        assertEquals("nick", comments.get(0).getNickName());
+        assertEquals("댓글을 수정합니다.", comments.get(0).getContent());
+    }
+
+    @Test
+    public void 댓글_삭제() throws Exception {
+        //given
+        Member member = createMember();
+        Long postId = postService.post(member.getId(), "test1", "게시물1", "게시물 작성합니다.");
+        Long commentId = commentService.comment(member.getMemberId(), postId, "댓글을 작성합니다.");
+
+        //when
+        commentService.deleteComment(commentId);
+        Comment findComment = commentRepository.findOne(commentId);
+
+        //then
+        assertEquals(null, findComment);
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setMemberId("eoruadl");
+        member.setPassword(bCryptPasswordEncoder.encode("1234"));
+        member.setNickName("nick");
+        member.setName("name");
+        member.setEmail("test@gmail.com");
+        em.persist(member);
+        return member;
+    }
+}
--- src/test/java/com/eoboard/service/PostServiceTest.java
+++ src/test/java/com/eoboard/service/PostServiceTest.java
@@ -3,27 +3,59 @@ package com.eoboard.service;
 import com.eoboard.domain.Member;
 import com.eoboard.domain.Post;
 import com.eoboard.repository.PostRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Transactional
+@AutoConfigureMockMvc
 public class PostServiceTest {
 
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
     PostService postService;
     @Autowired
     PostRepository postRepository;
     @Autowired
     EntityManager em;
+    @Autowired
+    ObjectMapper om;
+
+    @WithMockCustomUser
+    @Test
+    public void 게시물_작성_API() throws Exception {
+        Member member = createMember();
+
+        Map<String, String> body = new HashMap<>();
+        body.put("topic", "test");
+        body.put("title", "게시물 작성");
+        body.put("content", "게시물 작성 테스트다.");
+
+
+        mockMvc.perform(post("/api/v1/post")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(om.writeValueAsString(body)))
+                .andExpect(status().isOk());
+    }
 
     @Test
     public void 게시물_작성() throws Exception {
@@ -61,10 +93,54 @@ public class PostServiceTest {
         assertEquals(5, allPost.size());
     }
 
+    @Test
+    public void 게시물_조회() throws Exception {
+        //given
+        Member member = createMember();
+        Long postId = postService.post(member.getId(), "test1", "게시물1", "게시물 작성합니다.");
+
+        //when
+        Post findPost = postRepository.findOne(postId);
+
+        //then
+        assertEquals("test1", findPost.getTopic());
+        assertEquals("게시물1", findPost.getTitle());
+        assertEquals("게시물 작성합니다.", findPost.getContent());
+    }
+
+    @Test
+    public void 게시물_수정() throws Exception {
+        //given
+        Member member = createMember();
+        Long postId = postService.post(member.getId(), "test1", "게시물1", "게시물 작성합니다.");
+
+        //when
+        postService.updatePost(postId, "test1", "게시물수정", "게시물 수정합니다.");
+
+        //then
+        Post findPost = postRepository.findOne(postId);
+        assertEquals("게시물수정", findPost.getTitle());
+        assertEquals("게시물 수정합니다.", findPost.getContent());
+    }
+
+    @Test
+    public void 게시물_삭제() throws Exception {
+        //given
+        Member member = createMember();
+        Long postId = postService.post(member.getId(), "test1", "게시물1", "게시물 작성합니다.");
+
+        //when
+        postService.deletePost(postId);
+
+        //then
+        Post findPost = postRepository.findOne(postId);
+        assertEquals(null, findPost);
+    }
+
     private Member createMember() {
         Member member = new Member();
         member.setMemberId("eoruadl");
-        member.setPassword("1234");
+        member.setPassword(bCryptPasswordEncoder.encode("1234"));
         member.setNickName("nick");
         member.setName("name");
         member.setEmail("test@gmail.com");
